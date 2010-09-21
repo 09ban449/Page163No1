31ef335a9a8766afe494e6118c40352036fb6d82
Brandon <ban69@sage.southhills.edu>
2010-09-20 22:55:38 -0400

    Completed Program

--- Page163No1/frmEddiesEquipment.vb
+++ Page163No1/frmEddiesEquipment.vb
@@ -9,11 +9,62 @@
     End Sub
 
     Private Sub btnDisplayBill_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDisplayBill.Click
-        If IsNumeric(txtSelectItem) Then
+        Dim strItem As String
+        Dim strDuration As String
+        Dim dblPrice As Double
+        Dim dblDeposit As Double = 30.0
+        Dim fmtStr As String = "{0, -15} {1, -7} {2, -17}"
+        lstCustomerBill.Items.Clear()
+        If IsNumeric(txtSelectItem.Text) Then
             If CInt(txtSelectItem.Text) >= 1 And CInt(txtSelectItem.Text) <= 3 Then
-
+                Select Case CInt(txtSelectItem.Text)
+                    Case 1
+                        strItem = "Rug cleaner"
+                    Case 2
+                        strItem = "Lawn mower"
+                    Case 3
+                        strItem = "Paint sprayer"
+                End Select
             End If
+        Else
+            MessageBox.Show("Please Enter 1, 2, or 3!", "DUH!")
 
         End If
+
+        If txtSelectDuration.Text.ToUpper = "H" Or txtSelectDuration.Text.ToUpper = "F" Then
+            If txtSelectDuration.Text.ToUpper = "H" Then
+                strDuration = "Half-day"
+                Select Case CInt(txtSelectItem.Text)
+                    Case 1
+                        dblPrice = 16.0
+                    Case 2
+                        dblPrice = 12.0
+                    Case 3
+                        dblPrice = 20.0
+                End Select
+            Else
+                If txtSelectDuration.Text.ToUpper = "F" Then
+                    strDuration = "Full-day"
+                    Select Case CInt(txtSelectItem.Text)
+                        Case 1
+                            dblPrice = 24.0
+                        Case 2
+                            dblPrice = 18.0
+                        Case 3
+                            dblPrice = 30.0
+                    End Select
+                End If
+
+            End If
+        Else
+            MessageBox.Show("Please Enter H or F", "DUH!")
+        End If
+        lstCustomerBill.Items.Add("Receipt from Eddie's Equipment Rental")
+        lstCustomerBill.Items.Add("")
+        lstCustomerBill.Items.Add(String.Format(fmtStr, strItem, FormatCurrency(dblPrice), "(" & strDuration & " " & "rental" & ")"))
+        lstCustomerBill.Items.Add(String.Format(fmtStr, "Deposit", FormatCurrency(dblDeposit), ""))
+        lstCustomerBill.Items.Add("")
+        lstCustomerBill.Items.Add(String.Format(fmtStr, "Total", FormatCurrency(dblPrice + dblDeposit), ""))
+
     End Sub
 End Class
